13d412554fdac2de7693ebc9bb77d86601777367
xreip <pierre.mouffe@gmail.com>
2023-03-10 18:45:59 +0100

    SvelteKit template with tailwind and iconify

---
+++ svelte-tailwind/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-3xl font-bold text-orange-500">Welcome to SvelteKit</h1>
+<p class="font-bold">
+	Visit <a class="text-primary" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
